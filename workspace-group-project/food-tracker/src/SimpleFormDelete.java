c3b09a9bcd83c5d81c4b5cd3472fcbe867963581
devinkellogg <devin4646kellogg@gmail.com>
2020-04-05 19:35:15 -0500

    Cleaning things up

--- workspace-group-project/food-tracker/src/SimpleFormDelete.java
+++ workspace-group-project/food-tracker/src/SimpleFormDelete.java
@@ -54,9 +54,10 @@ public class SimpleFormDelete extends HttpServlet {
 
             "</ul>\n");
 
-      out.println("<a href=/webproject/simpleFormInsert.html>Insert Data</a> <br>");
-      out.println("<a href=/webproject/simpleFormSearch.html>Search Data</a> <br>");
-      out.println("<a href=/webproject/simpleFormDelete.html>Delete Data</a> <br>");
+      out.println("<a href=/webproject/home.html>Food Tracker Home</a> <br>");
+      out.println("<a href=/webproject/simpleFormInsert.html>Insert Order</a> <br>");
+      out.println("<a href=/webproject/simpleFormSearch.html>Search Order</a> <br>");
+      out.println("<a href=/webproject/simpleFormDelete.html>Delete Order</a> <br>");
       out.println("</body></html>");
    }
 
